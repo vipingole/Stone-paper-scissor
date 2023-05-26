2786d39cab760af2b00201a594ff38b603d2bc53
vipingole <91408566+vipingole@users.noreply.github.com>
2023-05-26 13:00:41 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,5 @@
+function submitForm() {
+    const selectedPet = document.querySelector('input[name="game"]:checked').value;
+    console.log(selectedPet);
+    // You can do something with the selected pet here
+  }
